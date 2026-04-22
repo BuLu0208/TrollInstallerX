3991097071278af8737bb0e356a050c20dfcd529
BuLu0208 <96172131+BuLu0208@users.noreply.github.com>
2026-04-22 17:09:39 +0800

    Add files via upload

--- TrollInstallerX/UI/MenuView.swift
+++ TrollInstallerX/UI/MenuView.swift
@@ -1,26 +1,25 @@
-//
+﻿//
 //  MenuView.swift
 //  TrollInstallerX
-//
-//  Created by Alfie on 22/03/2024.
 //
 
 import SwiftUI
 
 struct MenuView: View {
     @Binding var isShowingSettings: Bool
-    @Binding var isShowingCredits: Bool
     @Binding var isShowingMDCAlert: Bool
     @Binding var isShowingOTAAlert: Bool
     let device: Device
+    
     var body: some View {
         GeometryReader { geometry in
             ZStack {
-                RoundedRectangle(cornerRadius: 10)
-                    .foregroundColor(.white.opacity(0.15))
+                RoundedRectangle(cornerRadius: 16)
+                    .foregroundColor(Color.white.opacity(0.08))
+                
                 VStack {
                     Button(action: {
-                        if !isShowingCredits && !isShowingSettings && !isShowingMDCAlert && !isShowingOTAAlert {
+                        if !isShowingSettings && !isShowingMDCAlert && !isShowingOTAAlert {
                             UIImpactFeedbackGenerator().impactOccurred()
                             withAnimation {
                                 isShowingSettings = true
@@ -40,52 +39,17 @@ struct MenuView: View {
                                         .padding(.trailing, 5)
                                 }
                             )
-                            .foregroundColor(device.isSupported ? .white : .secondary)
+                            .foregroundColor(device.isSupported ? .white.opacity(0.85) : .secondary)
                             
                             Spacer()
                             
                             Image(systemName: "chevron.right")
                                 .font(.body)
-                                .foregroundColor(.white.opacity(0.5))
+                                .foregroundColor(.white.opacity(0.3))
                         }
                     })
                     .padding()
-                    .frame(maxHeight: geometry.size.height / 2)
-                    
-                    Divider()
-                    
-                    Button(action: {
-                        if !isShowingCredits && !isShowingSettings && !isShowingMDCAlert && !isShowingOTAAlert {
-                            UIImpactFeedbackGenerator().impactOccurred()
-                            withAnimation {
-                                isShowingCredits = true
-                            }
-                        }
-                    }, label: {
-                        HStack {
-                            Label(
-                                title: {
-                                    Text("关于")
-                                        .font(.system(size: 17, weight: .regular, design: .rounded))
-                                },
-                                icon: { Image(systemName: "info.circle")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                        .frame(width: 22, height: 22)
-                                        .padding(.trailing, 5)
-                                }
-                            )
-                            .foregroundColor(device.isSupported ? .white : .secondary)
-                            
-                            Spacer()
-                            
-                            Image(systemName: "chevron.right")
-                                .font(.body)
-                                .foregroundColor(.white.opacity(0.5))
-                        }
-                    })
-                    .padding()
-                    .frame(maxHeight: geometry.size.height / 2)
+                    .frame(maxWidth: .infinity)
                 }
                 .padding()
             }
